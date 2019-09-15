bcd88e5ff20259e29987fbcc98332c2201787f09
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2019-09-15 21:53:27 +0200

    first?

---
+++ prototype/src/js/app.js
@@ -0,0 +1,132 @@
+'use strict';
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookieMessage()
+{
+	if(getCookie('cookieConfirm') !== 'yes') {
+		document.getElementById('cookieMessage').classList.add('show');
+	}
+}
+
+function cookieAgree()
+{
+	setCookie('cookieConfirm', 'yes', 365);
+	document.getElementById('cookieMessage').classList.remove('show');
+}
+
+var freezeVp = function(e) {
+    e.preventDefault();
+};
+
+function stopBodyScrolling (bool) {
+    if (bool === true) {
+        document.body.addEventListener("touchmove", freezeVp, false);
+    } else {
+        document.body.removeEventListener("touchmove", freezeVp, false);
+    }
+}
+
+/**
+ * Parse url
+ */
+function urlParser($url)
+{	
+	var parser = document.createElement('a');
+	parser.href = $url;
+
+	var $result = parser.hostname;
+
+	return $result;
+}
+
+function slideTo(el)
+{
+	$('html, body').animate({
+		scrollTop: $(el).offset().top-60
+	}, 500);
+}
+
+function menuScroll()
+{
+	var $nav = $('.main-navigation');
+	var $scroll = $(window).scrollTop();
+	if($scroll >= 100) {
+		if(!$nav.hasClass('scrolled')) {
+			$nav.addClass('scrolled');
+		}
+	} else {
+		if($nav.hasClass('scrolled')) {
+			$nav.removeClass('scrolled');
+		}
+	}
+}
+
+function lazyImages()
+{
+	$('.lazyset').each(function() {
+		var $src = $(this).data('srcset');
+		$(this).attr('srcset', $src).removeAttr('data-src');	
+    });
+	$('.lazy').each(function() {
+		var $src = $(this).data('src');
+		$(this).attr('src', $src).removeAttr('data-src');	
+    });
+}
+
+$(document).ready(function() {
+
+});
+
+$(window).on('load', function() {
+
+	lazyImages();
+	var $owl = $('.owl-carousel');
+	$owl.owlCarousel({
+		loop: false,
+		rewind: true,
+		margin: 0,
+		nav: false,
+		dots: false,
+		responsive: {
+			0: {
+				items: 1
+			},
+			576: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+		}
+	});
+	
+	$('.owl-next').click(function() {
+		$owl.trigger('next.owl.carousel');
+	});
+	$('.owl-prev').click(function() {
+		$owl.trigger('prev.owl.carousel');
+	});
+
+});
