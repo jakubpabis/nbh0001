fa855c87d97f51afbac1ec0e16a020ba26fdeb00
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2019-09-17 18:20:43 +0200

    add product

--- prototype/src/js/app.js
+++ prototype/src/js/app.js
@@ -142,4 +142,28 @@ $(window).on('load', function() {
 		$owl.trigger('prev.owl.carousel');
 	});
 
+	var options = {
+		prefetch: true,
+		cacheLength: 10,
+		onStart: {
+			duration: 500, // Duration of our animation
+			render: function ($container) {
+				// Add your CSS animation reversing class
+				$container.addClass('is-exiting');
+				// Restart your animation
+				smoothState.restartCSSAnimations();
+			}
+		},
+		onReady: {
+			duration: 250,
+			render: function ($container, $newContent) {
+				// Remove your CSS animation reversing class
+				$container.removeClass('is-exiting');
+				// Inject the new content
+				$container.html($newContent);
+			}
+		}
+	},
+	smoothState = $('#wrapper').smoothState(options).data('smoothState');
+
 });
