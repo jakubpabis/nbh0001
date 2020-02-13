82e407c0de615843d46b1655696f080e5596d115
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2020-02-13 11:50:37 +0100

    should

--- build/wp-content/themes/sative/functions.php
+++ build/wp-content/themes/sative/functions.php
@@ -229,16 +229,6 @@ function sative_setup() {
 endif;
 add_action( 'after_setup_theme', 'sative_setup' );
 
-/**
- * 
- * Add WooCommerce support
- * 
- */
-add_action( 'after_setup_theme', 'woocommerce_support' );
-function woocommerce_support() 
-{
-    add_theme_support( 'woocommerce' );
-}
 // Remove all WooCommerce styles and scripts
 add_filter( 'woocommerce_enqueue_styles', '__return_false' );
 
