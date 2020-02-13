6601a0d3b5036d854b2c911ca926272e93b7af79
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2020-02-13 13:08:24 +0100

    agg

--- build/wp-content/themes/sative/woocommerce/content-single-product.php
+++ build/wp-content/themes/sative/woocommerce/content-single-product.php
@@ -373,7 +373,7 @@ do_action( 'woocommerce_single_product_summary' );
  * @hooked woocommerce_upsell_display - 15
  * @hooked woocommerce_output_related_products - 20
  
-do_action( 'woocommerce_after_single_product_summary' ); 
+do_action( 'woocommerce_after_single_product_summary' ); */
 ?>
 
 
