5a589c7c5d2d84719606c3f62df09c2a3eb83f4a
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2020-02-13 13:09:52 +0100

    asd

--- build/wp-content/themes/sative/woocommerce/content-single-product.php
+++ build/wp-content/themes/sative/woocommerce/content-single-product.php
@@ -365,15 +365,17 @@ if ( post_password_required() ) {
 
 
 <?php
+do_action( 'woocommerce_before_single_product_summary' );
 do_action( 'woocommerce_single_product_summary' );
 /*
  * Hook: woocommerce_after_single_product_summary.
  *
  * @hooked woocommerce_output_product_data_tabs - 10
  * @hooked woocommerce_upsell_display - 15
- * @hooked woocommerce_output_related_products - 20
+ * @hooked woocommerce_output_related_products - 2
+ */
  
-do_action( 'woocommerce_after_single_product_summary' ); */
+do_action( 'woocommerce_after_single_product_summary' ); 
 ?>
 
 
