cd26f02f806e970165d530deb17f8089aaa49a8c
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-02-15 22:56:26 +0100

    test5

--- build/wp-content/themes/sative/functions.php
+++ build/wp-content/themes/sative/functions.php
@@ -332,8 +332,8 @@ remove_action( 'admin_print_styles', 'print_emoji_styles' );
 add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
 function jk_dequeue_styles( $enqueue_styles ) {
 	if (!is_admin()) {
-		unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
-		//unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
+		//unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
+		unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
 		//unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
 		return $enqueue_styles;
 	}
