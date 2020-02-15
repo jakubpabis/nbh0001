3a808cc68de5f074aab09c639cfd688d48c13b41
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-02-15 13:35:35 +0100

    check

--- build/wp-content/themes/sative/functions.php
+++ build/wp-content/themes/sative/functions.php
@@ -540,6 +540,11 @@ add_action('wp_enqueue_scripts', function() {
 	wp_deregister_script('wp-emoji');
 });
 
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 ); 
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' ); 
+remove_action( 'wp_print_styles', 'print_emoji_styles' ); 
+remove_action( 'admin_print_styles', 'print_emoji_styles' )
+
 // /**
 //  * @snippet       Display "Quantity: #" @ WooCommerce Single Product Page
 //  * @how-to        Get CustomizeWoo.com FREE
