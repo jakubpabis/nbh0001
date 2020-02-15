d83fd8117af0e2da91e8f260302177af9022d5fc
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-02-15 18:01:44 +0100

    sdfg

--- build/wp-content/themes/sative/functions.php
+++ build/wp-content/themes/sative/functions.php
@@ -331,10 +331,12 @@ remove_action( 'admin_print_styles', 'print_emoji_styles' );
 // Remove each style one by one
 add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
 function jk_dequeue_styles( $enqueue_styles ) {
-	unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
-	//unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
-	//unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
-	return $enqueue_styles;
+	if (!is_admin()) {
+		unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
+		//unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
+		//unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
+		return $enqueue_styles;
+	}
 }
 
 // Or just remove them all in one line
