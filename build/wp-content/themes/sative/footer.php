49b46e4c4391539ef4ac0e2a1b9623fbabb521c1
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-02-17 00:58:43 +0100

    testing

--- build/wp-content/themes/sative/footer.php
+++ build/wp-content/themes/sative/footer.php
@@ -115,24 +115,8 @@
 	</div><!-- #wrapper -->
 	<!-- Load Facebook SDK for JavaScript -->
 	<div id="fb-root"></div>
-	<script>
-		window.fbAsyncInit = function() {
-			FB.init({
-				xfbml            : true,
-				version          : 'v6.0'
-			});
-		};
-		(function(d, s, id) {
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s); js.id = id;
-			js.src = 'https://connect.facebook.net/pl_PL/sdk/xfbml.customerchat.js';
-			fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
-	</script>
-
 	<!-- Your customer chat code -->
-		<div class="fb-customerchat"
+	<div class="fb-customerchat"
 		attribution=setup_tool
 		page_id="436289680462922"
 		theme_color="#F5A623"
