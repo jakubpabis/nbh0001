7b17a8543916d32633703bbe44f631a93995cd12
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-02-17 15:08:18 +0100

    add GA

--- build/wp-content/themes/sative/header-shop.php
+++ build/wp-content/themes/sative/header-shop.php
@@ -12,6 +12,14 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108874065-8"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-108874065-8');
+    </script>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
