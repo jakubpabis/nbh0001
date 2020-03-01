23dee22ddcf423ebc0b3861aaaf68f0b42155481
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-03-01 16:50:08 +0100

    team

---
+++ build/wp-content/themes/sative/tpl_team.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: Team
+ */
+
+get_header(); ?>
+
+<header class="team__header">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-auto text-center">
+                <h1>
+                    <?= get_field('team-title'); ?>
+                    Poznaj ziomeczków z naszego team'u!
+                </h1>
+            </div>
+        </div>
+    </div>
+</header>
+
+<?php if(have_rows('people')) : ?>
+<section class="team__people">
+    <div class="container">
+        <div class="row justify-content-center">
+            <?php while(have_rows('people')) : the_row(); ?>
+            <div class="col-lg-4 team__people-person">
+                <div class="person-img">
+                    <img src="<?= get_sub_field('img'); ?>" alt="" class="bg-cover">
+                </div>
+            </div>
+            <?php endwhile; ?>
+        </div>
+    </div>
+</section>
+<?php endif; ?>
+
+
+<?php get_footer();
